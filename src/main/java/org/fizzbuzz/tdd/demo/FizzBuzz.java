29ee2f324d52c3d7d4efb6ad91856b42efdbc18a
Akshat Agrawal <adroit@Akshats-MacBook-Air.local>
2021-04-15 15:51:04 +0530

    refactored the code

--- src/main/java/org/fizzbuzz/tdd/demo/FizzBuzz.java
+++ src/main/java/org/fizzbuzz/tdd/demo/FizzBuzz.java
@@ -3,7 +3,6 @@ package org.fizzbuzz.tdd.demo;
 public class FizzBuzz {
 
 	public String calculate(int number) {
-		// TODO Auto-generated method stub
 		String result = "";
 		
 		if (number % 3 == 0) {
--- src/test/java/org/fizzbuzz/tdd/demo/FizzBuzzTest.java
+++ src/test/java/org/fizzbuzz/tdd/demo/FizzBuzzTest.java
@@ -2,27 +2,32 @@ package org.fizzbuzz.tdd.demo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
+	private FizzBuzz fizzBuzz;
+	
+	@BeforeEach
+	public void init() {
+		fizzBuzz = new FizzBuzz();
+	}
+	
 	@Test
 	void testForFizz() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("fizz", fizzBuzz.calculate(3));
 		
 	}
 	
 	@Test
 	void testForBuzz() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("buzz", fizzBuzz.calculate(10));
 		
 	}
 	
 	@Test
 	void testForNeitherFizzNorBuzz() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("11", fizzBuzz.calculate(11));
 		
 	}
